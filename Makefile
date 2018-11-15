75df6744610049c46d9ff9bc75055eda4d5064fb
jsta <stachel2@msu.edu>
2018-11-15 08:36:47 -0500

    add figures pdf

--- Makefile
+++ Makefile
@@ -7,7 +7,12 @@ data: data/gis.gpkg
 data/gis.gpkg: scripts/00_get_gis.R
 	Rscript $<
 
-figures: data figures/size_comparison.pdf
+figures: data manuscript/figures.pdf
+
+manuscript/figures.pdf: manuscript/figures.Rmd figures/size_comparison.pdf
+	Rscript -e "rmarkdown::render('$<', output_format = 'pdf_document')"
+	-pdftk manuscript/figures.pdf cat 2-end output manuscript/figures2.pdf
+	-mv manuscript/figures2.pdf manuscript/figures.pdf
 
 figures/size_comparison.pdf: scripts/03_viz.R
 	Rscript $<
--- scripts/03_viz.R
+++ scripts/03_viz.R
@@ -19,12 +19,26 @@ res        <- dplyr::bind_rows(res) %>%
   tidyr::gather(key = "scale", value = "area", -q)
 
 
-theme_opts <- theme_minimal()
+theme_opts <- theme_minimal() +
+  theme(axis.title = element_text(size = 16),
+        axis.text = element_text(size = 16),
+        legend.text = element_text(size = 14))
 g <- ggplot() +
   geom_line(data = res, aes(x = area, y = rev(q), color = scale), size = 1.5) +
   ylab("Density") + xlab("Area (m2)") +
   scale_x_continuous(labels = c("10^2", "10^11"),
                      breaks = c(10^2, 10^11)) +
-  theme_opts
+  theme_opts +
+  labs(color = "")
 ggsave("figures/size_comparison.pdf")
+
+# ---- ag_practice_line-plots ----
+
+# library(macroag)
+#
+# data("tillage_ctic")
+# tillage_ctic <- dplyr::filter(tillage_ctic, huc8_n %in% as.character(hu8s$HUC8))
+#
+# plot(tillage_ctic$totacre, tillage_ctic$notill)
+# abline(0, 1)
 
